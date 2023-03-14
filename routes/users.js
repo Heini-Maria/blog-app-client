08a3f4739583f8c35c1730bd9f0526418ca10044
Heini-Maria Kotipelto <115211431+Heini-Maria@users.noreply.github.com>
2023-03-14 10:32:58 +0100

    fixes

--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const { Users } = require("../models");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 const { sign } = require("jsonwebtoken");
 const { validateToken } = require("../middleware/authMiddleware");
 const { check, validationResult } = require('express-validator');
 
 router.post("/", async (req, res) => {
   const { username, password } = req.body;
-  const user = await Users.findOne({ where: { username: username } });
-  if(!user) {
     await bcrypt.hash(password, 10).then((hash) => {
-      Users.create({
+       Users.create({
         username: username,
         password: hash,
       });
-      return res.json("SUCCESS");
+      return res.json("success");
     });
-  }
-  return alert("username already exists")
   });
 
 router.post("/login", async (req, res) => {
